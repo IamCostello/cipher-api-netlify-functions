73f95cc6f5943b860d3f132986f1d00897c25c60
IamCostello <mynameiscostello@gmail.com>
2021-03-04 17:08:11 +0100

    Add crack ceasar cipher service

--- src/services/ceasarCipher.ts
+++ src/services/ceasarCipher.ts
@@ -1,3 +1,4 @@
+import LanguageDetect from "languagedetect";
 import getAscii from "./helpers/getAscii";
 import { ASCII, ASCII_START } from "../config/ascii/ascii";
 
@@ -14,4 +15,38 @@ export const cipher = (text: string, shift: number): string => {
 
 export const decipher = (text: string, shift: number): string => {
   return cipher(text, ASCII - (shift % ASCII));
+};
+
+export const getAllCiphers = (text: string) => {
+  const ciphers = [];
+
+  for (let i = 0; i < ASCII; i++) {
+    ciphers.push(cipher(text, ASCII - i));
+  }
+
+  return ciphers;
+};
+
+export const crackCipher = (text: string) => {
+  const lngDetector = new LanguageDetect();
+  lngDetector.setLanguageType("iso2");
+
+  let max = 0;
+  let language = "";
+  let message = "";
+
+  for (let i = 0; i < ASCII; i++) {
+    const decipheredText = cipher(text, ASCII - i);
+    const [score] = lngDetector.detect(decipheredText, 1);
+
+    if (score) {
+      if (score[1] > max && score[0] !== null) {
+        max = score[1];
+        language = score[0];
+        message = decipheredText;
+      }
+    }
+  }
+
+  return [language, max, message];
 };
